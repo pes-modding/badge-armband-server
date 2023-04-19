b8fe223fedf2eec4f2b0b589cfe030a03503be67
Anton Jouline <juce66@gmail.com>
2023-04-19 09:42:20 -0700

    left/right/home-specific badges logic reworked

--- modules/SleeveBadge-ArmbandServer.lua
+++ modules/SleeveBadge-ArmbandServer.lua
@@ -2,18 +2,24 @@
 -- by Hawke and juce
 -- originally released on EvoWeb.uk in April 2023
 
-local m = { version = "v1.1" }
+local m = { version = "v2.1" }
 
 local map
 local map_count
+local team_map
 local content_root
 local patterns
 local armband_patch_addr
 local badge_patch_addr
 local badge_patch_addr2
-local badge0_left_ftex
+local hard_patch_addr
+local hard_patch_codecave_addr
+local badge_patch_map
 local messages = {}
 local frame_count = 0
+
+-- function handlers
+local get_badge
 
 local reload_button = {
     vkey = 0x30, label = "[0]"
@@ -22,6 +28,18 @@ local reload_button = {
 local empty = {}
 local function get_common_lib(ctx)
     return ctx.common_lib or empty
+end
+
+if ffi ~= nil then
+    -- bind VirtualAlloc, unless it is already bound
+    ffi.cdef[[
+    typedef uint64_t LPVOID;
+    typedef uint64_t SIZE_T;
+    typedef uint32_t DWORD;
+    typedef uint8_t BYTE;
+
+    BYTE* VirtualAlloc(LPVOID lpAddress, SIZE_T dwSize, DWORD  flAllocationType, DWORD  flProtect);
+    ]]
 end
 
 local function len(t)
@@ -44,14 +62,46 @@ local function get_tid(ctx)
 end
 
 function m.get_filepath(ctx, filename)
+    local str = string.match(filename, "Asset\\model\\character\\uniform\\badge\\#windx11\\badge(%d+)%.ftex")
+    if str then
+        local badge_id = tonumber(str)
+        local tid = get_tid(ctx)
+        local folder = map[tid]
+        if not folder then
+            -- no badges mapped for this tournament id
+            if badge_id >= 0x2000 then
+                badge_id = badge_id - 0x2000
+            end
+            if badge_id >= 0x1000 then
+                badge_id = badge_id - 0x1000
+            end
+            if badge_id ~= tonumber(str) then
+                filename = string.format("Asset\\model\\character\\uniform\\badge\\#windx11\\badge%02d.ftex", badge_id)
+                log("Loading: " .. filename .. " (" .. str .. ")")
+                return ret
+            end
+            log("Loading: " .. filename)
+            return
+        end
+        log("Loading: " .. filename)
+        local path = get_badge(ctx, folder, str)
+        log(filename .. " => " .. tostring(path))
+        return path
+    end
+
     for p,f in pairs(patterns) do
         local str = string.match(filename, p)
         if str then
+            log("Loading: " .. filename)
+            --if code_cave_addr then
+            --    local is_away_team = memory.unpack("u8", memory.read(code_cave_addr + 0x20, 1))
+            --    log(string.format("is_away_team: %d", is_away_team))
+            --end
             local tid = get_tid(ctx)
             local folder = map[tid]
             if not folder then
                 -- nothing mapped for this tournament id
-                return 
+                return
             end
             local path = f(ctx, folder, str)
             log(filename .. " => " .. tostring(path))
@@ -81,74 +131,155 @@ local function unset_badge()
 end
 
 local function set_badge_left()
-    --[[
-    0000000141EAD060 | 83E7 01                         | and edi,1                               |
-    0000000141EAD063 | 66:83C7 62                      | add di,62                               |
-    0000000141EAD067 | 90                              | nop                                     |
-    0000000141EAD068 | 90                              | nop                                     |
-    0000000141EAD069 | 90                              | nop                                     |
-    --]]
     log("applying badge-left patch at " .. memory.hex(badge_patch_addr2))
-    memory.write(badge_patch_addr2, "\x83\xe7\x01\x66\x83\xc7\x62\x90\x90\x90")
+    memory.write(badge_patch_addr2, "\x90\x90\x90\x90\x90")
 end
 
 local function unset_badge_left()
-    --[[
-    0000000141EAD060 | 0FB77C24 7C                     | movzx edi,word ptr ss:[rsp+7C]          |
-    0000000141EAD065 | 66:85FF                         | test di,di                              |
-    0000000141EAD068 | 74 53                           | je pes2021.141EAD0BD                    |
-    --]]
     log("removing badge-left patch at " .. memory.hex(badge_patch_addr2))
-    memory.write(badge_patch_addr2, "\x0f\xb7\x7c\x24\x7c\x66\x85\xff\x74\x53")
-    badge0_left_ftex = nil
+    memory.write(badge_patch_addr2, "\x66\x85\xff\x74\x53")
 end
+
+local function set_harl()
+    --[[
+    0000000141EAD731 | 48:B8 0000450C00000000          | mov rax,C450000                         |
+    0000000141EAD73B | FFD0                            | call rax                                |
+    0000000141EAD73D | 90                              | nop                                     |
+    0000000141EAD73E | 90                              | nop                                     |
+    0000000141EAD73F | 90                              | nop                                     |
+    --]]
+    log("applying home-away-right-left patch at " .. memory.hex(harl_patch_addr))
+    memory.write(harl_patch_addr, "\x48\xb8" .. memory.pack("u64", harl_patch_codecave_addr) .. "\xff\xd0\x90\x90\x90")
+    --[[
+    000000000C450000 | 41:80FC 01                      | cmp r12b,1                              |
+    000000000C450004 | 75 05                           | jne C45000B                             |
+    000000000C450006 | 66:81C1 0010                    | add cx,1000                             |
+    000000000C45000B | 48:B8 80D0EA4101000000          | mov rax,pes2021.141EAD080               |
+    000000000C450015 | 48:398424 80000000              | cmp qword ptr ss:[rsp+80],rax           |
+    000000000C45001D | 72 05                           | jb C450024                              |
+    000000000C45001F | 66:81C1 0020                    | add cx,2000                             |
+    000000000C450024 | 48:33C0                         | xor rax,rax                             |
+    000000000C450027 | 0FB7C1                          | movzx eax,cx                            |
+    000000000C45002A | 894424 28                       | mov dword ptr ss:[rsp+28],eax           |
+    000000000C45002E | 4C:8BC3                         | mov r8,rbx                              | r8:"badge00"
+    000000000C450031 | 48:8D4C24 38                    | lea rcx,qword ptr ss:[rsp+38]           |
+    000000000C450036 | 8D53 21                         | lea edx,qword ptr ds:[rbx+21]           |
+    000000000C450039 | C3                              | ret                                     |
+    --]]
+    log("writing home-away-right-left code snippet at " .. memory.hex(harl_patch_codecave_addr))
+    memory.write(harl_patch_codecave_addr, 
+        "\x41\x80\xfc\01" ..
+        "\x75\x05" ..
+        "\x66\x81\xc1\x00\x10" ..
+        "\x48\xb8" .. memory.pack("u64", badge_patch_addr2 + 0x80 - 0x65) ..
+        "\x48\x39\x84\x24\x80\x00\x00\x00" ..
+        "\x72\x05" ..
+        "\x66\x81\xc1\x00\x20" ..
+        "\x48\x33\xc0" ..
+        "\x0f\xb7\xc1" ..
+        "\x89\x44\x24\x28" ..
+        "\x4c\x8b\xc3" ..
+        "\x48\x8d\x4c\x24\x38" ..
+        "\x8d\x53\x21" ..
+        "\xc3")
+end
+
+local function unset_harl()
+    --[[
+    0000000141EAD731 | 894424 20                       | mov dword ptr ss:[rsp+20],eax           |
+    0000000141EAD735 | 4C:8BC3                         | mov r8,rbx                              | r8:"badge00"
+    0000000141EAD738 | 48:8D4C24 30                    | lea rcx,qword ptr ss:[rsp+30]           |
+    0000000141EAD73D | 8D53 21                         | lea edx,qword ptr ds:[rbx+21]           |
+    --]]
+    log("removing home-away-right-left patch at " .. memory.hex(harl_patch_addr))
+    memory.write(harl_patch_addr, "\x89\x44\x24\x20\x4c\x8b\xc3\x48\x8d\x4c\x24\x30\x8d\x53\x21")
+end
+
+badge_patch_map = {
+    ["badge0.ftex"] = set_badge,
+    ["badge1.ftex"] = set_badge,
+    ["badge2.ftex"] = set_badge,
+    ["badge0-left.ftex"] = set_badge_left,
+    ["badge1-left.ftex"] = set_badge_left,
+    ["badge2-left.ftex"] = set_badge_left,
+}
 
 local function get_armband(ctx, folder, basename)
-    return content_root .. "\\" .. folder .. "\\cap\\" .. basename
+    return content_root .. folder .. "\\cap\\" .. basename
 end
 
-local function get_badge(ctx, folder, badge_id)
+local function get_full_pathname(ctx, folder, is_away, badge_file, suffix)
+    local team_id
+    if is_away then
+        team_id = ctx.away_team
+    else
+        team_id = ctx.home_team
+    end
+    local team_folder = team_map[team_id]
+    -- check mapping
+    if not team_folder then
+        return content_root .. folder .. "\\badge\\" .. badge_file .. suffix
+    end
+    -- check file existence
+    local pathname = content_root .. folder .. "\\badge\\" .. team_folder .. "\\" .. badge_file .. suffix
+    local f = io.open(pathname)
+    if f then
+        f:close()
+        return pathname
+    end
+    return content_root .. folder .. "\\badge\\" .. badge_file .. suffix
+end
+
+get_badge = function(ctx, folder, badge_id)
+    local is_away, is_left = false, false
     badge_id = tonumber(badge_id)
+    if badge_id >= 0x2000 then
+        badge_id = badge_id - 0x2000
+        is_left = true
+    end
+    if badge_id >= 0x1000 then
+        badge_id = badge_id - 0x1000
+        is_away = true
+    end
+    log(string.format("Loading badge: %d, is_left=%s, is_away=%s", badge_id, is_left, is_away))
+    local suffix = is_left and "-left.ftex" or ".ftex"
+
     if badge_id == 0 then
-        return content_root .. "\\" .. folder .. "\\badge\\badge0.ftex"
-    elseif badge_id == 98 then
-        if badge0_left_ftex then
-            return content_root .. "\\" .. folder .. "\\badge\\badge0-left.ftex"
-        end
-        return content_root .. "\\" .. folder .. "\\badge\\badge2-left.ftex"
-    elseif badge_id == 99 then
-        return content_root .. "\\" .. folder .. "\\badge\\badge1-left.ftex"
+        return get_full_pathname(ctx, folder, is_away, "badge0", suffix)
     end
     -- one of the two badges is typically a champions badge
     if badge_id and badge_id % 2 == 0 then
         -- 2nd badge
-        return content_root .. "\\" .. folder .. "\\badge\\badge2.ftex"
+        return get_full_pathname(ctx, folder, is_away, "badge2", suffix)
     elseif badge_id then
         -- 1st badge
-        return content_root .. "\\" .. folder .. "\\badge\\badge1.ftex" 
+        return get_full_pathname(ctx, folder, is_away, "badge1", suffix)
     end
 end
 
 local function get_respect_badge(ctx, folder)
-    return content_root .. "\\" .. folder .. "\\badge\\respect_badge.ftex"
+    return content_root .. folder .. "\\badge\\respect_badge.ftex"
 end
 
-local function load_map(filename)
+local function load_map(filename, required)
     local map = {}
     local delim = ","
     local f = io.open(filename)
     if not f then
-        error("unable to open " .. filename .. " for reading")
+        if required then
+            error("unable to open " .. filename .. " for reading")
+        end
+        log("WARN: unable to open " .. filename .. " for reading. Skipping")
     end
     f:close()
     for line in io.lines(filename) do
         -- trim comments and whitespace
         line = line:gsub("#.*$", "")
-        local tournament_id, folder = string.match(line, "%s*(.+)%s*,%s*[\"]([^\"]+)[\"]")
-        tournament_id = tonumber(tournament_id)
-        if tournament_id and folder then
-            map[tournament_id] = folder
-            log(string.format("map: tournament %d => %s", tournament_id, folder))
+        local id, folder = string.match(line, "%s*(.+)%s*,%s*[\"]([^\"]+)[\"]")
+        id = tonumber(id)
+        if id and folder then
+            map[id] = folder
+            log(string.format("map: id %d => %s", id, folder))
         end
     end
     log("total entries in map: " .. len(map)) 
@@ -166,7 +297,8 @@ end
 
 function m.key_down(ctx, vkey)
     if vkey == reload_button.vkey then
-        map = load_map(content_root .. "map.txt")
+        map = load_map(content_root .. "map.txt", true)
+        team_map = load_map(content_root .. "map_teams.txt")
         messages[#messages + 1] = "map reloaded"
         frame_count = 0
     end
@@ -177,6 +309,7 @@ function m.set_teams(ctx)
     unset_uefa_armband()
     unset_badge()
     unset_badge_left()
+    unset_harl()
     local tid = get_tid(ctx)
     local folder = map[tid]
     if not folder then
@@ -195,44 +328,27 @@ function m.set_teams(ctx)
             set_uefa_armband()
         end
     end
-    local badge = get_badge(ctx, folder, 0)
-    if badge then
-        local f = io.open(badge)
-        if f then
-            f:close()
-            log(string.format("current tournament id %d has a custom badge: %s", tid, badge))
-            -- we have a custom badge for this tournament, so patch the exe
-            -- to enforce the loading of it
-            set_badge()
-        end
-        badge = string.gsub(badge, "badge0.ftex", "badge0-left.ftex")
-        f = io.open(badge)
-        if f then
-            f:close()
-            badge0_left_ftex = true
-            log(string.format("current tournament id %d has a custom left sleeve badge: %s", tid, badge))
-            -- we have a custom badge for this tournament, so patch the exe
-            -- to enforce the loading of it
-            set_badge_left()
+    local team_folders = {
+        comp = "",
+        home = team_map[ctx.home_team],
+        away = team_map[ctx.away_team],
+    }
+    for filename, patch_func in pairs(badge_patch_map) do
+        for _, team_folder in pairs(team_folders) do
+            team_folder = team_folder == "" and "" or team_folder .. "\\"
+            local fname = content_root .. folder .. "\\badge\\" .. team_folder .. filename
+            local f = io.open(fname)
+            if f then
+                f:close()
+                log(string.format("current tournament id %d has a custom badge: %s", tid, fname))
+                patch_func()
+                has_a_badge = true
+            end
         end
     end
-    -- check licensed case
-    local badge_left
-    if not badge0_left_ftex then
-        badge_left = get_badge(ctx, folder, 98)
-    end
-    if not badge_left then
-        badge_left = get_badge(ctx, folder, 99)
-    end
-    if badge_left then
-        local f = io.open(badge_left)
-        if f then
-            f:close()
-            log(string.format("current tournament id %d has a custom badge for left sleeve: %s", tid, badge_left))
-            -- we have a custom left sleeve badge for this tournament, so patch the exe
-            -- to enforce the loading of it
-            set_badge_left()
-        end
+    -- if mapped, then we the harl-patch
+    if has_a_badge then
+        set_harl()
     end
 end
 
@@ -264,8 +380,35 @@ function m.init(ctx)
     0000000141EAD060 | 0FB77C24 7C                     | movzx edi,word ptr ss:[rsp+7C]          |
     0000000141EAD065 | 66:85FF                         | test di,di                              |
     --]]
-    badge_patch_addr2 = badge_patch_addr + 0x60 - 0x09
+    badge_patch_addr2 = badge_patch_addr + 0x65 - 0x09
     log("badge (left shoulder) logic found at: " .. memory.hex(badge_patch_addr2))
+
+    --[[
+    0000000141EAD717 | 49:8BF8                         | mov rdi,r8                              | rdi:"badge00", r8:"badge00"
+    0000000141EAD71A | 48:8BF2                         | mov rsi,rdx                             |
+    0000000141EAD71D | 66:83F9 64                      | cmp cx,64                               | 64:'d'
+    0000000141EAD721 | 73 6F                           | jae pes2021.141EAD792                   |
+    0000000141EAD723 | 0FB7C1                          | movzx eax,cx                            |
+    --]]
+    harl_patch_addr = memory.search_process("\x49\x8b\xf8\x48\x8b\xf2\x66\x83\xf9\x64\x73\x6f\x0f\xb7\xc1")
+    if not harl_patch_addr then
+        error("unable to find code location for harl patch")
+    end
+    harl_patch_addr = harl_patch_addr + 0x31 - 0x17
+    log("harl patch location found at: " .. memory.hex(harl_patch_addr))
+
+    -- allocate a code-cave
+    if ffi then
+        local allocationFlags = 0x1000 + 0x2000
+        local protection_execute_readwrite = 0x40
+        harl_patch_codecave_addr = ffi.C.VirtualAlloc(nil, 512, allocationFlags, protection_execute_readwrite)
+        if not harl_patch_codecave_addr then
+            error("unable to allocate memory with VirtualAlloc")
+        end
+        log("harl_patch_codecave_addr allocated at: " .. memory.hex(harl_patch_codecave_addr))
+    else
+        error("ffi module must be enabled for this module to work. To enable, set luajit.ext.enabled = 1 in sider.ini")
+    end
 
     -- check for CommonLib
     if not ctx.common_lib then
@@ -273,9 +416,10 @@ function m.init(ctx)
     end
 
     content_root = ctx.sider_dir .. "content\\badge-server\\"
-    map = load_map(content_root .. "map.txt")
+    map = load_map(content_root .. "map.txt", true)
+    team_map = load_map(content_root .. "map_teams.txt")
     patterns = {
-        ["Asset\\model\\character\\uniform\\badge\\#windx11\\badge(%d+)%.ftex"] = get_badge,
+        --["Asset\\model\\character\\uniform\\badge\\#windx11\\badge(%d+)%.ftex"] = get_badge,
         ["Asset\\model\\character\\uniform\\badge\\#windx11\\respect_badge%.ftex"] = get_respect_badge,
         ["Asset\\model\\character\\uniform\\cap\\#windx11\\(CL_captainmark.+)"] = get_armband,
     }
